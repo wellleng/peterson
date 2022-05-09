d6dabf968dcaafc11571c250d85b08619cc0df92
wellleng <wellleng@github.com>
2022-05-09 20:07:58 +0800

    'init'

---
+++ src/main/java/tech/hehan/peterson/controller/HelloController.java
@@ -0,0 +1,13 @@
+package tech.hehan.peterson.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Li Hao love He Han!";
+    }
+}
