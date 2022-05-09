769a1d28bd80d9814ff13eccfce740d4edb36ce1
wellleng <wellleng@github.com>
2022-05-09 20:35:48 +0800

    update the pom

--- src/main/java/tech/hehan/peterson/controller/HelloController.java
+++ src/main/java/tech/hehan/peterson/controller/HelloController.java
@@ -1,6 +1,5 @@
 package tech.hehan.peterson.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
